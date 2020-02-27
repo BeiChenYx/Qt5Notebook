47414625a5daae992beb6a0ac09675bd7d7f9a1b
yx-mac <yangxing2297@163.com>
2020-02-27 23:00:13 +0800

    修正了StockKVLine的坐标近似值的问题，完成了数据统一获取，数据清除，数据添加的功能

--- QWidgetNotebook/CustomWidget/StockKVLine/KVLines.py
+++ QWidgetNotebook/CustomWidget/StockKVLine/KVLines.py
@@ -9,7 +9,7 @@ from PyQt5.QtChart import QChartView, QChart, QLineSeries, QLegend, QCategoryAxi
 from PyQt5.QtCore import Qt, QPointF, QPoint, pyqtSignal, QRectF, QMargins, QEvent, QObject
 from PyQt5.QtGui import QPainter, QPen, QColor, QMouseEvent, QFont
 from PyQt5.QtWidgets import QApplication, QGraphicsLineItem, QWidget, QLabel, QHBoxLayout, QVBoxLayout, QGridLayout, \
-    QGraphicsProxyWidget, QSplitter
+    QGraphicsProxyWidget, QSplitter, QPushButton
 
 
 def read_tick_data():
@@ -21,34 +21,6 @@ def read_tick_data():
     sw_50 = pd.read_csv('sw_daily_201907162109.csv', encoding='utf-8')
     sw_50['trade_date'] = sw_50['trade_date'].apply(lambda x: str(x))
     return sw_50
-
-
-def min_approximate(value):
-    """ 根据给的值向下近似一定量的值 """
-    if value >= 100000:
-        return value * 0.999
-    elif 10000 < value < 100000:
-        return value * 0.99
-    elif 1000 < value < 10000:
-        return value * 0.99
-    elif 10 < value < 1000:
-        return value - 10
-    else:
-        return value
-
-
-def max_approximate(value):
-    """ 根据给定的值向上近似一定量的值 """
-    if value >= 100000:
-        return value * 1.001
-    elif 10000 < value < 100000:
-        return value * 1.01
-    elif 1000 < value < 10000:
-        return value * 1.10
-    elif 10 < value < 1000:
-        return value + 10
-    else:
-        return value
 
 
 class ToolTipWidget(QWidget):
@@ -95,6 +67,7 @@ class ToolTipWidget(QWidget):
         self.label_high_str.setText(highT)
         self.label_low_str.setText(lowT)
 
+
 class GraphicsProxyWidget(QGraphicsProxyWidget):
     """ ToolTipWidget的代理Widget """
     def __init__(self, *args, **kwargs):
@@ -119,13 +92,14 @@ class GraphicsProxyWidget(QGraphicsProxyWidget):
 
 class KLineChartView(QChartView):
 
-    def __init__(self):
+    def __init__(self, data: pd.DataFrame):
         super(KLineChartView, self).__init__()
         self.setRenderHint(QPainter.Antialiasing)  # 抗锯齿
         self._chart = QChart()
-        self.stocks = read_tick_data()
-        self.category = [trade_date[4:] for trade_date in self.stocks['trade_date']]
-        self._count = len(self.category)
+        self._series = QCandlestickSeries()
+        self._stocks = data
+        self._category = list()
+        self._count = None
         self._zero_point = QPointF(0, 0)
         self._max_point = QPointF(0, 0)
         self.resize(800, 300)
@@ -134,34 +108,60 @@ class KLineChartView(QChartView):
         self._zero_value = (0, self._chart.axisY().min())
         self._max_value = (len(self._chart.axisX().categories()), self._chart.axisY().max())
         # 计算x轴单个cate的宽度，用来处理横线不能画到边界
-        self.cate_width = (self._max_point.x() - self._zero_point.x()) / len(self.category)
+        self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
+
+    def set_name(self, name):
+        self._series.setName(name)
+
+    def clear_series_values(self):
+        self._series.clear()
+        self._chart.axisY().setRange(0, 10)
+        self._chart.axisX().setCategories(list())
+        self._stocks = None
+
+    def add_series_values(self, data: pd.DataFrame, is_init=False):
+        self._stocks = data
+        self._category = [trade_date[4:] for trade_date in self._stocks['trade_date']]
+        self._count = len(self._category)
+        for _, stock in self._stocks.iterrows():
+            time_p = datetime.datetime.strptime(stock['trade_date'], '%Y%m%d')
+            time_p = float(time.mktime(time_p.timetuple()))
+            _set = QCandlestickSet(float(stock['open']), float(stock['high']),
+                                   float(stock['low']), float(stock['close']), time_p, self._series)
+            # _set.hovered.connect(self.handleBarHoverd)  # 鼠标悬停
+            self._series.append(_set)
+
+        if not is_init:
+            axis_x = self._chart.axisX()
+            axis_y = self._chart.axisY()
+            axis_x.setCategories(self._category)
+            max_p = self._stocks[['high', 'low']].stack().max()
+            min_p = self._stocks[['high', 'low']].stack().min()
+            axis_y.setRange(min_p * 0.99, max_p * 1.01)
+            self._zero_value = (0, self._chart.axisY().min())
+            self._max_value = (len(self._chart.axisX().categories()), self._chart.axisY().max())
+            # 计算x轴单个cate的宽度，用来处理横线不能画到边界
+            self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
 
     def resizeEvent(self, event):
         super(KLineChartView, self).resizeEvent(event)
         self._zero_point = self._chart.mapToPosition(QPointF(self._zero_value[0], self._zero_value[1]))
         self._max_point = self._chart.mapToPosition(QPointF(self._max_value[0], self._max_value[1]))
-        self.cate_width = (self._max_point.x() - self._zero_point.x()) / len(self.category)
+        self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
 
     def max_point(self):
-        return QPointF(self._max_point.x() - self.cate_width / 2, self._max_point.y())
+        return QPointF(self._max_point.x() - self._cate_width / 2, self._max_point.y())
 
     def min_point(self):
-        return QPointF(self._zero_point.x() - self.cate_width / 2, self._zero_point.y())
+        return QPointF(self._zero_point.x() - self._cate_width / 2, self._zero_point.y())
 
     def init_chart(self):
         self._chart.setAnimationOptions(QChart.SeriesAnimations)
-        series = QCandlestickSeries()
-        series.setIncreasingColor(QColor(Qt.red))
-        series.setDecreasingColor(QColor(Qt.green))
-        series.setName(self.stocks['name'].iloc[0])
-        for _, stock in self.stocks.iterrows():
-            time_p = datetime.datetime.strptime(stock['trade_date'], '%Y%m%d')
-            time_p = float(time.mktime(time_p.timetuple()))
-            _set = QCandlestickSet(float(stock['open']), float(stock['high']),
-                                   float(stock['low']), float(stock['close']), time_p, series)
-            # _set.hovered.connect(self.handleBarHoverd)  # 鼠标悬停
-            series.append(_set)
-        self._chart.addSeries(series)
+        self._series.setIncreasingColor(QColor(Qt.red))
+        self._series.setDecreasingColor(QColor(Qt.green))
+        self._series.setName(self._stocks['name'].iloc[0])
+        self.add_series_values(self._stocks, True)
+        self._chart.addSeries(self._series)
 
         self._chart.createDefaultAxes()
         self._chart.setLocalizeNumbers(True)
@@ -169,11 +169,11 @@ class KLineChartView(QChartView):
         axis_y = self._chart.axisY()
         axis_x.setGridLineVisible(False)
         axis_y.setGridLineVisible(False)
-        axis_x.setCategories(self.category)
+        axis_x.setCategories(self._category)
         axis_x.setLabelsVisible(False)
-        max_p = self.stocks[['high', 'low']].stack().max()
-        min_p = self.stocks[['high', 'low']].stack().min()
-        axis_y.setRange(min_approximate(min_p), max_approximate(max_p))
+        max_p = self._stocks[['high', 'low']].stack().max()
+        min_p = self._stocks[['high', 'low']].stack().min()
+        axis_y.setRange(min_p * 0.99, max_p * 1.01)
 
         # chart的图例
         legend = self._chart.legend()
@@ -190,11 +190,13 @@ class KLineChartView(QChartView):
 
 
 class VLineChartView(QChartView):
-    def __init__(self):
+    def __init__(self, data: pd.DataFrame):
         super(VLineChartView, self).__init__()
-        self.stocks = read_tick_data()
-        self.category = [trade_date[4:] for trade_date in self.stocks['trade_date']]
+        self._stocks = data
+        self._category = [trade_date[4:] for trade_date in self._stocks['trade_date']]
         self._chart = QChart()
+        self._chart.setAnimationOptions(QChart.SeriesAnimations)
+        self._series = QStackedBarSeries()
         self._zero_point = QPointF(0, 0)
         self._max_point = QPointF(0, 0)
         # 成交量以万股为单位
@@ -204,29 +206,21 @@ class VLineChartView(QChartView):
         self._zero_value = (0, self._chart.axisY().min())
         self._max_value = (len(self._chart.axisX().categories()), self._chart.axisY().max())
         # 计算x轴单个cate的宽度，用来处理横线不能画到边界
-        self.cate_width = (self._max_point.x() - self._zero_point.x()) / len(self.category)
+        self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
 
-    def resizeEvent(self, event):
-        super(VLineChartView, self).resizeEvent(event)
-        self._zero_point = self._chart.mapToPosition(QPointF(self._zero_value[0], self._zero_value[1]))
-        self._max_point = self._chart.mapToPosition(QPointF(self._max_value[0], self._max_value[1]))
-        self.cate_width = (self._max_point.x() - self._zero_point.x()) / len(self.category)
+    def clear_series_value(self):
+        self._series.clear()
+        self._stocks = None
+        self._chart.axisY().setRange(0, 10)
+        self._chart.axisX().setCategories(list())
 
-    def max_point(self):
-        return QPointF(self._max_point.x() - self.cate_width / 2, self._max_point.y())
-
-    def min_point(self):
-        return QPointF(self._zero_point.x() - self.cate_width / 2, self._zero_point.y())
-
-    def init_chart(self):
-        self._chart.setAnimationOptions(QChart.SeriesAnimations)
-        series = QStackedBarSeries()
-        series.setName('数量')
+    def add_series_values(self, data: pd.DataFrame, is_init=False):
+        self._stocks = data
         bar_red = QBarSet('red')
         bar_red.setColor(Qt.red)
         bar_green = QBarSet('green')
         bar_green.setColor(Qt.green)
-        for _, stock in self.stocks.iterrows():
+        for _, stock in self._stocks.iterrows():
             if stock['open'] < stock['close']:
                 bar_red.append(stock['vol'] / self._vol_multiple)
                 bar_green.append(0)
@@ -234,9 +228,36 @@ class VLineChartView(QChartView):
                 bar_red.append(0)
                 bar_green.append(stock['vol'] / self._vol_multiple)
 
-        series.append(bar_red)
-        series.append(bar_green)
-        self._chart.addSeries(series)
+        self._series.append(bar_red)
+        self._series.append(bar_green)
+
+        if not is_init:
+            axis_x = self._chart.axisX()
+            axis_y = self._chart.axisY()
+            axis_x.setCategories(self._category)
+            max_p = self._stocks[['vol', ]].stack().max()
+            min_p = self._stocks[['vol', ]].stack().min()
+            axis_y.setRange(min_p / self._vol_multiple * 0.9, max_p / self._vol_multiple * 1.1)
+            self._zero_value = (0, self._chart.axisY().min())
+            self._max_value = (len(self._chart.axisX().categories()), self._chart.axisY().max())
+            # 计算x轴单个cate的宽度，用来处理横线不能画到边界
+            self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
+
+    def resizeEvent(self, event):
+        super(VLineChartView, self).resizeEvent(event)
+        self._zero_point = self._chart.mapToPosition(QPointF(self._zero_value[0], self._zero_value[1]))
+        self._max_point = self._chart.mapToPosition(QPointF(self._max_value[0], self._max_value[1]))
+        self._cate_width = (self._max_point.x() - self._zero_point.x()) / len(self._category)
+
+    def max_point(self):
+        return QPointF(self._max_point.x() - self._cate_width / 2, self._max_point.y())
+
+    def min_point(self):
+        return QPointF(self._zero_point.x() - self._cate_width / 2, self._zero_point.y())
+
+    def init_chart(self):
+        self.add_series_values(self._stocks, True)
+        self._chart.addSeries(self._series)
         self._chart.createDefaultAxes()
         self._chart.setLocalizeNumbers(True)
         axis_x = self._chart.axisX()
@@ -244,11 +265,11 @@ class VLineChartView(QChartView):
         axis_x.setGridLineVisible(False)
         axis_y.setGridLineVisible(False)
         axis_y.setLabelFormat("%.2f")
-        axis_x.setCategories(self.category)
+        axis_x.setCategories(self._category)
         axis_x.setLabelsVisible(False)
-        max_p = self.stocks[['vol', ]].stack().max()
-        min_p = self.stocks[['vol', ]].stack().min()
-        axis_y.setRange(min_approximate(min_p / self._vol_multiple), max_approximate(max_p / self._vol_multiple))
+        max_p = self._stocks[['vol', ]].stack().max()
+        min_p = self._stocks[['vol', ]].stack().min()
+        axis_y.setRange(min_p / self._vol_multiple * 0.9, max_p / self._vol_multiple * 1.1)
 
         # chart的图例
         legend = self._chart.legend()
@@ -268,16 +289,33 @@ class KVWidget(QWidget):
     def __init__(self):
         super(KVWidget, self).__init__()
         self.setMouseTracking(True)
-        self.k_view = KLineChartView()
-        self.v_view = VLineChartView()
+        # 获取数据
+        self.stocks = read_tick_data()
+
+        self.k_view = KLineChartView(self.stocks)
+        self.v_view = VLineChartView(self.stocks)
         self.resize(800, 600)
         # self.showMaximized()
+
+        btn_widget = QWidget()
+        h_layout = QHBoxLayout()
+        btn_clear = QPushButton('清除')
+        btn_add = QPushButton('添加')
+        h_layout.addStretch()
+        h_layout.addWidget(btn_clear)
+        h_layout.addWidget(btn_add)
+        btn_widget.setLayout(h_layout)
+
+        btn_clear.clicked.connect(self.on_clear_clicked)
+        btn_add.clicked.connect(self.on_add_clicked)
 
         self.v_splitter = QSplitter(Qt.Vertical, self)
         self.v_splitter.addWidget(self.k_view)
         self.v_splitter.addWidget(self.v_view)
+        self.v_splitter.addWidget(btn_widget)
         self.v_splitter.setStretchFactor(0, 3)
-        self.v_splitter.setStretchFactor(1, 1)
+        self.v_splitter.setStretchFactor(1, 2)
+        self.v_splitter.setStretchFactor(2, 1)
 
         layout = QVBoxLayout()
         # layout.setContentsMargins(0, 0, 0, 0)
@@ -344,9 +382,8 @@ class KVWidget(QWidget):
                 self.k_line_v.hide()
                 self.v_line_v.hide()
                 self.v_line_h.hide()
-            # TODO: 数据统一在主页面管理，提供灵活的接口设置和清除替换数据
-            # TODO: 使用自绘的方式在横坐标下面写日期，并且只显示指定个数的时间, K线图隐藏横坐标轴，V线图显示横坐标轴即可
             # TODO: 添加浮动框, 在K线里面添加所有信息，包括成交量的信息
+            # TODO: 使用自绘的方式在横坐标下面写日期，并且只显示指定个数的时间, K线图隐藏横坐标轴，V线图显示横坐标轴即可
             # TODO: 在给定日期的K线图上绘制文字
             # TODO: 向左右拖动图表能显示之前或之后的图表，且坐标跟着变化
             # TODO: 能标准成本线，能计算指定两个点的涨幅度
@@ -355,6 +392,9 @@ class KVWidget(QWidget):
 
     def resizeEvent(self, event):
         super(KVWidget, self).resizeEvent(event)
+        self.update_margins()
+
+    def update_margins(self):
         margin_k = self.k_view.chart().margins()
         margin_v = self.v_view.chart().margins()
         width_k = self.k_view.chart().plotArea().width()
@@ -366,6 +406,22 @@ class KVWidget(QWidget):
         else:
             self.v_view.chart().setMargins(QMargins(margin_v.left() - sub, margin_v.top(),
                                                     margin_v.right(), margin_v.bottom()))
+        self.update()
+
+    def on_clear_clicked(self):
+        """ 清空图表 """
+        print('on clear clicked')
+        self.k_view.clear_series_values()
+        self.v_view.clear_series_value()
+        self.k_view.set_name('')
+
+    def on_add_clicked(self):
+        """ 添加数据 """
+        print('on add clicked')
+        self.k_view.add_series_values(self.stocks)
+        self.v_view.add_series_values(self.stocks)
+        self.k_view.set_name(self.stocks['name'].iloc[0])
+        self.update_margins()
 
 
 if __name__ == "__main__":
